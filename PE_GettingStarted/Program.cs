cba0397aa98f7acfbdb15afba5b369aaeaad8091
Erin Cascioli <edcigm@rit.edu>
2025-09-03 12:42:39 -0400

    Added my name to the PE
    Kept HelloWorld console statement.
    Added my name as a second console statement.

---
+++ PE_GettingStarted/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PE_GettingStarted
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Erin Cascioli");
+        }
+    }
+}
