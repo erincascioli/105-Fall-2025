2cae6786109f23d4814106507a95d1422f5b20ae
Erin Cascioli <edcigm@rit.edu>
2025-09-03 16:47:26 -0400

    Comment practice

--- IntroDemo/Program.cs
+++ IntroDemo/Program.cs
@@ -4,6 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // Introduction to the user
+            // I tend to do this when I code
+            // And have a lot of single line comments
+
+            /* This is a multi line comment
+             * every time you press enter a new line appears
+             * that is started with an automartic asterisk
+             * star-slash ends the comment  */
             Console.WriteLine("Hello World!");
             Console.WriteLine("jfzgxdjhfvbzsjgxbljxrskgzsbjd");
         }
