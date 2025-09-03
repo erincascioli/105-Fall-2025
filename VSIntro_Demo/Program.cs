2b8cfaa11452ceda69915db87b17b7b3035b5869
Erin Cascioli <edcigm@rit.edu>
2025-09-03 16:30:05 -0400

    Demo for class on how to create the right type of program

---
+++ VSIntro_Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace VSIntro_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("waghkduhgxkub");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("sfjbSjcbsajhfsD");
+            Console.WriteLine("");
+        }
+    }
+}
