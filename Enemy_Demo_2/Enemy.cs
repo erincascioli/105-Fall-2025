a67b70ce4026f43ae229ab7b9f9cbf11d685a701
Erin Cascioli <edcigm@rit.edu>
2025-11-05 15:02:33 -0500

    Pushing Enemy code with properties
    Includes properties for name, current health, and whether the enemy is dead.

---
+++ Enemy_Demo_2/Enemy.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enemy_Demo_2
+{
+    public enum Weapon
+    {
+        Sword,
+        Axe,
+        Dagger
+    }
+
+    internal class Enemy
+    {
+        // --------------------------------------------------------------------
+        // FIELDS of the class
+        // --------------------------------------------------------------------
+        public string name;
+        public int currentHealth;
+        public const int MaxHealth = 100;
+        public Weapon enemyWeapon;
+        public string[] heldItems;
+
+
+        // --------------------------------------------------------------------
+        // CONSTRUCTOR of the class
+        // --------------------------------------------------------------------
+
+        // TODO: Add an XML comment for this constructor
+        public Enemy(string name, int currentHealth)
+        {
+            // Did an empty name get passed in as the param? 
+            if (name.Length == 0)
+            {
+                this.name = "Bob";
+            }
+            else
+            {
+                this.name = name;
+            }
+
+            if (currentHealth > MaxHealth)
+            {
+                this.currentHealth = MaxHealth;
+            }
+            else
+            {
+                this.currentHealth = currentHealth;
+            }
+
+            // Weapon type is dependent on the name length
+            int nameLength = name.Length;
+
+            if (nameLength >= 1 && nameLength <= 3)         // 1-3 chars
+            {
+                enemyWeapon = Weapon.Sword;
+            }
+            else if (nameLength <= 7)            // 4-7 chars
+            {
+                enemyWeapon = Weapon.Axe;
+            }
+            else                                // 8+ chars
+            {
+                enemyWeapon = Weapon.Dagger;
+            }
+
+            // All enemies start with the same 3 items
+            heldItems = new string[3] { "rations", "trinket", "trap" };
+        }
+
+
+        // --------------------------------------------------------------------
+        // METHODS of the class
+        // --------------------------------------------------------------------
+
+        /// <summary>
+        /// Print information about the Enemy.
+        /// </summary>
+        public void PrintEnemy()
+        {
+            // Printing to the console: name, current health/max health, 
+            //     weapon, a list of the items they hold
+            Console.WriteLine($"{name} has {currentHealth}/{maxHealth} hit points.");
+            Console.WriteLine($"{name} wields a {enemyWeapon}.");
+            Console.WriteLine($"Items in {name}'s bag are:");
+            for(int i = 0; i < heldItems.Length; i++)
+            {
+                Console.WriteLine($" - {heldItems[i]}");
+            }
+        }
+
+        /// <summary>
+        /// Displays health information about an Enemy.
+        /// </summary>
+        public void PrintHealthInformation()
+        {
+            Console.WriteLine($"{name} has {currentHealth}/{MaxHealth} hit points.");
+        }
+
+        /// <summary>
+        /// Harms an Enemy for a specific number of hit points
+        /// </summary>
+        /// <param name="damage">Hit points of damage</param>
+        public void TakeDamage(int damage)
+        {
+            currentHealth -= Math.Abs(damage);
+        }
+
+        /// <summary>
+        /// Heals the enemy for a specific number of hit points
+        /// </summary>
+        /// <param name="healPoints">Points to heal the enemy</param>
+        public void Heal(int healPoints)
+        {
+            currentHealth += Math.Abs(healPoints);
+        }
+
+        // Write an attack method dependent on the type of weapon and also if I'm at 
+        //   100% health, possible bonus crit attack of 1 - 5.
+
+    }
+}
---
+++ Properties_Demo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Properties_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            // Instantiate 2 objects of the Enemy class
+            Enemy firstEnemy = new Enemy("Godzilla", 75);
+            Enemy secondEnemy = new Enemy("Mothra", 100);
+
+            // This method prints the class name of the first enemy
+            // We'll look at how to override this next week!
+            //Console.WriteLine(firstEnemy.ToString());
+
+            // Print the enemy stats
+            firstEnemy.PrintEnemy();
+            Console.WriteLine("---------------------------------------");
+            secondEnemy.PrintEnemy();
+            Console.WriteLine("---------------------------------------\n\n");
+
+            // Take some damage, then reprint stats for confirmation of damage taken
+            Console.WriteLine($"----- {secondEnemy.Name} ATTACKS {firstEnemy.Name} -----");
+            int attackDamage = secondEnemy.Attack();
+            Console.WriteLine("The enemy attacks for " + attackDamage);
+            firstEnemy.TakeDamage(attackDamage);
+            firstEnemy.PrintEnemy();
+
+            // The enemy runs across a health pack and heals to full health
+            Console.WriteLine($"\n\n----- {firstEnemy.Name} FINDS A HEALTH PACK -----");
+            firstEnemy.HealToFullHealth();
+            firstEnemy.PrintEnemy();
+
+            // ------------------------------------------------------------------------------------
+            // Calling Properties
+            // ------------------------------------------------------------------------------------
+
+            // The Name property does not have a set block, so reassignment of its value cannot occur.
+            //firstEnemy.Name = "szjkvbxdfjkxd";        // NOOOO! INACCESSIBLE!
+
+            // The set block of the CurrentHealth peoperty is being called here.
+            // Since CurrentHealth has a set block, this code is syntactically correct.
+            firstEnemy.CurrentHealth = 1;
+            firstEnemy.PrintEnemy();
+
+            // The IsDeadYet property returns whether they character is dead as a boolean.
+            // This syntax calls the get block.
+            bool enemyIsDead = firstEnemy.IsDeadYet;
+            Console.WriteLine($"Is {firstEnemy.IsDeadYet} dead? {enemyIsDead}.");
+
+            // The set block of the CurrentHealth peoperty is being called here.
+            // Since CurrentHealth has a set block, this code is syntactically correct.
+            // HOWEVER, 194726 is not being set as the value in the property, because it's
+            //   outside the range of 0 - maxHealth.
+            firstEnemy.CurrentHealth = 194726;
+
+            // So when we print it, the enemy's health is still 1.
+            firstEnemy.PrintEnemy();
+        }
+    }
+}
